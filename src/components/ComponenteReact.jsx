586487659a0bac443a1c46624027639f40825df2
Luis Angel Hernandez <rlwev@hotmail.com>
2023-09-08 17:23:58 -0500

    fin del sitio en Astro e integramos React en el proyecto

---
+++ src/components/ComponenteReact.jsx
@@ -0,0 +1,10 @@
+import React, {useState} from 'react'
+
+export default function ComponenteReact() {
+    const [cliente, setCliente] = useState('Luis')
+
+    console.log('Cliente y servidor')
+  return (
+    <div>{cliente}</div>
+  )
+}
